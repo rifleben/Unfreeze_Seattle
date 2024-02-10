295892feca6242801eb92fb72f6576fc32ad6c85
rifleben <riflemab@oregonstate.edu>
2024-02-09 17:38:06 -0800

    implementation of cards and json

--- server/app.js
+++ server/app.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const cardJSON = require('./content/cards.json');
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
+// // Serve static files from the React app
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
-// The "catchall" handler: for any request that doesn't
-// match one above, send back the React app's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// // The "catchall" handler: for any request that doesn't
+// // match one above, send back the React app's index.html file.
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname + '/client/build/index.html'));
+// });
+
+
+// serve JSON data:
+
+
+app.get("/", (req, res) => {
+  res.json(cardJSON);
 });
 
 const port = process.env.PORT || 3000;
