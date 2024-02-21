e95983094014bc5a7ec52808f52b1b6c705b19c0
rifleben <riflemab@oregonstate.edu>
2024-02-20 17:14:24 -0800

    Pulling in data from the backend to the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,53 @@
 import React from "react";
+import { useState, useEffect } from "react";
 import NoteCard from "./components/NoteCard/NoteCard";
 import CustomButton from "./components/NoteCard/CustomButton";
+import axios from "axios";
 
 import "./App.css";
 import styles from "./components/NoteCard/CustomButton.module.css";
 
 
+
 function App() {
+
+  const [data, setData] = useState([]);
+  const [index, setIndex] = useState(0);
+
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/notecards')
+      .then((response) => {
+        setData(response.data);
+        console.log(response.data);
+      }).catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const nextQuestion = () => {
+
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex + 1;
+      if (newIndex > data.notecards.length - 1) {
+        return 0;
+      }
+      return newIndex;
+    })
+  }
+
+  const prevQuestion = () => {
+    setIndex((prevIndex) => {
+      const newIndex = prevIndex - 1;
+      if (newIndex < 0) {
+        return data.notecards.length - 1;
+      }
+      return newIndex;
+    })
+  }
+
+  const cardText = data.notecards ? data.notecards[index].text : "Loading...";
+
   return (
     <div>
       <div className="container">
@@ -14,13 +55,13 @@ function App() {
           <h1>Seattle Unfreeze</h1>
         </div>
         <NoteCard>
-          <p>This is the content of the flashcard.</p>
+          <p>{cardText}</p>
         </NoteCard>
         <div className={styles.buttonContainer}>
-          <CustomButton onClick={() => console.log('Prev Question')}>
+          <CustomButton onClick={prevQuestion}>
             <i className="bi bi-arrow-left-square-fill"></i> Previous Question
           </CustomButton>
-          <CustomButton onClick={() => console.log('Next Question')}>
+          <CustomButton onClick={nextQuestion}>
             Next Question <i className="bi bi-arrow-right-square-fill"></i>
           </CustomButton>
         </div>
--- server/app.js
+++ server/app.js
@@ -2,14 +2,16 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const cardJSON = require('./content/cards.json');
+const cors = require('cors');
 // serve JSON data:
 
+app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/notecards", (req, res) => {
   res.json(cardJSON);
 });
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
 });
