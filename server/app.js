5c8309a7d25d05b2738d01536ef2e446de9994c2
rifleben <riflemab@oregonstate.edu>
2024-03-05 18:46:46 -0800

    port

--- server/app.js
+++ server/app.js
@@ -3,18 +3,18 @@ const path = require('path');
 const app = express();
 const cardJSON = require('./content/cards.json');
 const cors = require('cors');
-// serve JSON data:
 
 app.use(cors());
 
-//serve static files:
+//serve react built files:
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-
+//serve json content:
 app.get("/notecards", (req, res) => {
   res.json(cardJSON);
 });
 
+// use process.env.PORT for cloud deployment
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
